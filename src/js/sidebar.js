d3e7b8fe216741c209b015fc17085d1ef2ab210c
NEORIS\nicolas.kuhn <nicolas.kuhn@neoris.com>
2022-11-17 14:41:49 -0300

    finish dashboard screen

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,6 +1,5 @@
 const sidebarHandler = document.querySelector(".sidebar-handler");
 
-
 sidebarHandler.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("scroll-paused")
     document.querySelector(".sidebar").classList.toggle("active")
